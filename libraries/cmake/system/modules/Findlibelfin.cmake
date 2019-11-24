a92378c156a71ff7b28b2c75299d0da60f0993e2
Anatol Pomozov <anatol.pomozov@gmail.com>
2019-11-24 09:38:32 -0800

    Use system libraries

--- libraries/cmake/system/modules/Findlibelfin.cmake
+++ libraries/cmake/system/modules/Findlibelfin.cmake
@@ -4,4 +4,9 @@
 # This source code is licensed in accordance with the terms specified in
 # the LICENSE file found in the root directory of this source tree.
 
-include("${CMAKE_SOURCE_DIR}/libraries/cmake/source/modules/Findlibelfin.cmake")
+locateSystemLibrary(
+  NAME libelfin
+  REQUIRED ${libelfin_REQUIRED}
+  MAIN_HEADER "libelfin/elf/elf++.hh" "libelfin/dwarf/dwarf++.hh"
+  LIBRARIES "dwarf++" "elf++"
+)
